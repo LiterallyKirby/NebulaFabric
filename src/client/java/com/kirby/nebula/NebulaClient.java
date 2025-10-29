aa269ba3fccebf3f5312b57d40f43d0c4ade2610
LiterallyKirby <soniciscool1232@gmail.com>
2025-10-29 09:04:20 -0500

    setting up enviorment

---
+++ src/client/java/com/kirby/nebula/NebulaClient.java
@@ -0,0 +1,10 @@
+package com.kirby.nebula;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NebulaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
